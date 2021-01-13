a2ccb1569dad810b87658023782b1ea30dc7a3ff
saharit09 <6206021620167@fitm.kmutnb.ac.th>
2021-01-13 11:03:32 +0700

    create

---
+++ Lab4_1.php
@@ -0,0 +1,23 @@
+<html>
+<head><title>การกำหนดค่าตัวแปร</title></head>
+<body>
+     <?php
+         $x = 100;
+         $y = "200";
+         $z = '300';
+         $add = $x + $y;
+         $sub = $z - $x;
+         $f = $y * $z;
+         $s = $f / $z;
+         $message = "Hello World";
+         echo $x; echo "<br />";
+         echo $y; echo "<br />";
+         echo $z; echo "<br />";
+         echo $add; echo "<br />";
+         echo $sub; echo "<br />";
+         echo $f; echo "<br />";
+         echo $s; echo "<br />";
+         echo $message; echo "<br />";
+     ?>
+</body>
+</html>
